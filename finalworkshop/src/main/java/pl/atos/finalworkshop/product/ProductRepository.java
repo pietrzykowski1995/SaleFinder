84b2f5dfcc78acbff9267902a632462f847ecf84
pietrzykowski1995 <pietrzykowski1995@gmail.com>
2019-07-25 15:48:59 +0200

    add login form and controller

--- finalworkshop/src/main/java/pl/atos/finalworkshop/product/ProductRepository.java
+++ finalworkshop/src/main/java/pl/atos/finalworkshop/product/ProductRepository.java
@@ -16,11 +16,11 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategoryId(Long id);
 
-    @Query(nativeQuery = true, value = "select * from products " +
-            " inner join shop on products.shop_id=shop.id" +
-            " inner join shop_city on shop.id=shop_city.shopList_id" +
-            " inner join city on city.id=shop_city.cities_id" +
-            " where city.name = :cityName and products.name = :productName")
-    List<Product> findByCityAndName(@Param("cityName") String cityName, @Param("productName") String productName);
+        @Query(nativeQuery = true, value = "select * from products " +
+                " inner join shop on products.shop_id=shop.id" +
+                " inner join shop_city on shop.id=shop_city.shopList_id" +
+                " inner join city on city.id=shop_city.cities_id" +
+                " where city.name = :cityName and products.name = :productName")
+        List<Product> findByCityAndName(@Param("cityName") String cityName, @Param("productName") String productName);
 }
 
